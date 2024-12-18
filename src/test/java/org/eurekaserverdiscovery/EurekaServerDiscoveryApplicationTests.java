b9f61666360f8e29cb414608cbb6ac50126aa484
Minaoui Mohamed <minaouimh@gmail.com>
2024-12-18 18:06:32 +0100

    first commit

---
+++ src/test/java/org/eurekaserverdiscovery/EurekaServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eurekaserverdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
